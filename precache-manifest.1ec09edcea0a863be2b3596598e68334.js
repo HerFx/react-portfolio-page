6b3aeac926bf7e118061afcef4b79c9b0a6f8e5b
Mikołaj Janiak <janiak.m2222@gmail.com>
2022-10-11 19:57:20 +0200

    Updates

---
+++ precache-manifest.1ec09edcea0a863be2b3596598e68334.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8921639865c32644815",
+    "url": "/react-portfolio-page/static/css/main.f64b2c41.chunk.css"
+  },
+  {
+    "revision": "a8921639865c32644815",
+    "url": "/react-portfolio-page/static/js/main.c9b21a31.chunk.js"
+  },
+  {
+    "revision": "f885fc76a9f9b07d5ca0",
+    "url": "/react-portfolio-page/static/js/runtime~main.c626576f.js"
+  },
+  {
+    "revision": "a44b7418f6860af59c0b",
+    "url": "/react-portfolio-page/static/css/2.c47ffba1.chunk.css"
+  },
+  {
+    "revision": "a44b7418f6860af59c0b",
+    "url": "/react-portfolio-page/static/js/2.b8597199.chunk.js"
+  },
+  {
+    "revision": "8163f13eb499f5c0d529",
+    "url": "/react-portfolio-page/static/js/3.8d74271d.chunk.js"
+  },
+  {
+    "revision": "40e7b8c0ab4ed4af0a8de24fd5e515b2",
+    "url": "/react-portfolio-page/index.html"
+  }
+];
